9662a7cbfabe9a4517fd525ed96591d74ec62f1a
rahulk789 <rahul.u.india@gmail.com>
2022-08-09 08:52:47 +0530

    xdp.c compiles

--- XDPgo/xdp.c
+++ XDPgo/xdp.c
@@ -1,13 +1,24 @@
 // +build ignore
-#include "bpf_endian.h"
-#include "common.h"
+#define KBUILD_MODNAME "xdp_filter"
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/sched.h>
-
+#include <linux/kernel.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/tty.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+struct bpf_map_def SEC("maps") counter = {
+      .type = BPF_MAP_TYPE_ARRAY,
+      .key_size = sizeof(__u32),
+      .value_size = sizeof(long),
+      .max_entries = 256,
+};
 SEC("xdp")
-BPF_HISTOGRAM(counter, u64);
-
 int xdp_filter(struct xdp_md *ctx) {
   void *data = (void *)(long)ctx->data;
   void *data_end = (void *)(long)ctx->data_end;
@@ -22,9 +33,11 @@ int xdp_filter(struct xdp_md *ctx) {
 
         struct tcphdr *tcp = (void *)ip + sizeof(*ip);
         if ((void *)tcp + sizeof(*tcp) <= data_end) {
-          u64 value = htons(tcp->dest);
+          __u64 value = htons(tcp->dest);
           // u64 source = ntohs(tcp->dest);
-          counter.increment(value);
+//          counter.Put(value);
+          //if (value)
+              //__sync_fetch_and_add(value, 1);
           if (value == 4040) // && x != -1)
             return XDP_PASS;
           else if (value != 4040) // && x != -1 )
