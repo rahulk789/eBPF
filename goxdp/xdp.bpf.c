9bf8f266b26a6b763f2c249c460869bad8e62388
rahulk789 <rahul.u.india@gmail.com>
2022-08-19 20:57:55 +0530

    added map_array

--- goxdp/xdp.bpf.c
+++ goxdp/xdp.bpf.c
@@ -5,17 +5,20 @@
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 struct {
-	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(type, BPF_MAP_TYPE_ARRAY);
 	__uint(key_size, sizeof(u32));
 	__uint(value_size, sizeof(u32));
+    __uint(max_entries, 1);
 } events SEC(".maps");
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 
 int getp(void *ctx)
 {   char d[100];
-    bpf_get_current_comm(&d, sizeof(d));
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &d, sizeof(d));
+  int key=0;
+   bpf_get_current_comm(&d, sizeof(d));
+    //valp= bpf_map_lookup_elem(&events,&key);
+  bpf_map_update_elem(&events,&key,&d,BPF_ANY);
     return 0;
 }
 
@@ -35,7 +38,12 @@ int xdp_filter(struct xdp_md *ctx) {
         struct tcphdr *tcp = (void *)ip + sizeof(*ip);
         if ((void *)tcp + sizeof(*tcp) <= data_end) {
           u64 value = (tcp->dest);
-//          counter.Put(value);
+//          char d[100];
+ //         bpf_get_current_comm(&d, sizeof(d));
+    //valp= bpf_map_lookup_elem(&events,&key);
+ //         bpf_map_update_elem(&events,&value,&d,BPF_ANY);
+
+          //          counter.Put(value);
           //if (value)
             //  __sync_fetch_and_add(value, 1);
           if (value == 4040)//  && x != "myprocess" )
