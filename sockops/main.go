63521a42902a05c55d025ba16aa49c3af7244a0e
rahulk789 <rahul.u.india@gmail.com>
2022-11-26 19:21:45 +0000

    fixed blah blah blah idk

---
+++ sockops/main.go
@@ -0,0 +1,61 @@
+
+//go:build linux
+// +build linux
+
+package main
+
+import (
+	"time"
+    "log"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang bpf ./sk.bpf.c -- -I/usr/include/bpf -I.
+
+func main() {
+	// Allow the current process to lock memory for eBPF resources.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
+
+	// Get the first-mounted cgroupv2 path.
+	cgroupPath := "/sys/fs/cgroup/cg1"
+
+	// Link the count_egress_packets program to the cgroup.
+	l, err := link.AttachCgroup(link.CgroupOptions{
+		Path:    cgroupPath,
+		Attach:  ebpf.AttachCGroupInetEgress,
+		Program: objs.CountEgressPackets,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	log.Println("Counting packets...")
+
+	// Read loop reporting the total amount of times the kernel
+	// function was entered, once per second.
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		var value uint64
+		if err := objs.PktCount.Lookup(uint32(0), &value); err != nil {
+			log.Fatalf("reading map: %v", err)
+		}
+		log.Printf("number of packets: %d\n", value)
+	}
+}
+
+		// example fields: cgroup2 /sys/fs/cgroup/unified cgroup2 rw,nosuid,nodev,noexec,relatime 0 0
+
---
+++ sockops/sk.bpf.c
@@ -0,0 +1,50 @@
+// +build ignore
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct bpf_map_def {
+	unsigned int type;
+	unsigned int key_size;
+	unsigned int value_size;
+	unsigned int max_entries;
+	unsigned int map_flags;
+};
+
+struct bpf_map_def SEC("maps") pkt_count = {
+	.type        = BPF_MAP_TYPE_ARRAY,
+	.key_size    = sizeof(__u32),
+	.value_size  = sizeof(__u64),
+	.max_entries = 1,
+};
+
+SEC("cgroup_skb/egress")
+int count_egress_packets(struct __sk_buff *skb) {
+	__u32 key      = 0;
+	__u64 init_val = 1;
+	u64 *count = bpf_map_lookup_elem(&pkt_count, &key);
+	if (!count) {
+		bpf_map_update_elem(&pkt_count, &key, &init_val, BPF_ANY);
+		return 1;
+	}
+	__sync_fetch_and_add(count, 1);
+    int proto_num,src_port,dest_port;
+    struct iphdr *ih = ip_hdr(skb);
+    proto_num = ih->protocol;
+    switch (ih->protocol)
+    {
+    case IPPROTO_TCP:
+    {
+        struct tcphdr *th = tcp_hdr(skb);
+        src_port = th->source;
+        dest_port = th->dest;
+        break;
+    }
+    default:
+        src_port = 0;
+        dest_port = 0;
+    }
+	return 1;
+}
+
